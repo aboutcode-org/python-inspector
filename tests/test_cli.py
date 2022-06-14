da7057f6901819600aec81c138ddb67cf07cb0e0
Philippe Ombredanne <pombredanne@nexb.com>
2022-06-14 17:14:59 +0200

    Refine CLI testing
    * Make it data driven and test the whole result againsta  JSON file
    * Add new test for a requirements files
    * Use regeneration of fixtures with the PYINSP_REGEN_TEST_FIXTURES=yes
      environment variable.
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,46 +9,154 @@
 # See https://aboutcode.org for more information about nexB OSS projects.
 #
 
+import json
+import os
+
 import pytest
 from click.testing import CliRunner
+from commoncode.testcase import FileDrivenTesting
 
-from python_inspector.resolve_cli import resolve_dependencies as cli
+from python_inspector.resolve_cli import resolve_dependencies
+
+# Used for tests to regenerate fixtures with regen=True
+REGEN_TEST_FIXTURES = os.getenv("PYINSP_REGEN_TEST_FIXTURES", False)
+
+test_env = FileDrivenTesting()
+test_env.test_data_dir = os.path.join(os.path.dirname(__file__), "data")
 
 
 @pytest.mark.online
-def test_cli_with_json_api():
-    runner = CliRunner()
-    result = runner.invoke(
-        cli,
-        ["--spec", "zipp==3.8.0", "--json", "-"],
+def test_cli_with_default_urls():
+    expected_file = test_env.get_test_loc("default-url-expected.json", must_exist=False)
+    specifier = "zipp==3.8.0"
+    check_specs_resolution(
+        specifier=specifier,
+        expected_file=expected_file,
+        regen=REGEN_TEST_FIXTURES,
     )
-    assert result.exit_code == 0
 
 
 @pytest.mark.online
 def test_cli_with_single_index_url():
-    runner = CliRunner()
-    result = runner.invoke(
-        cli,
-        ["--spec", "zipp==3.8.0", "--index-url", "https://pypi.org/simple", "--json", "-"],
+    expected_file = test_env.get_test_loc("single-url-expected.json", must_exist=False)
+    specifier = "zipp==3.8.0"
+    extra_options = [
+        "--index-url",
+        "https://pypi.org/simple",
+    ]
+    check_specs_resolution(
+        specifier=specifier,
+        expected_file=expected_file,
+        extra_options=extra_options,
+        regen=REGEN_TEST_FIXTURES,
     )
-    assert result.exit_code == 0
 
 
 @pytest.mark.online
 def test_cli_with_multiple_index_url_and_tilde_req():
-    runner = CliRunner()
-    result = runner.invoke(
-        cli,
-        [
-            "--spec",
-            "zipp~=3.8.0",
-            "--index-url",
-            "https://pypi.org/simple",
-            "--index-url",
-            "https://thirdparty.aboutcode.org/pypi/simple/",
-            "--json",
-            "-",
-        ],
+    expected_file = test_env.get_test_loc("tilde_req-expected.json", must_exist=False)
+    specifier = "zipp~=3.8.0"
+    extra_options = [
+        "--index-url",
+        "https://pypi.org/simple",
+        "--index-url",
+        "https://thirdparty.aboutcode.org/pypi/simple/",
+    ]
+    check_specs_resolution(
+        specifier=specifier,
+        expected_file=expected_file,
+        extra_options=extra_options,
+        regen=REGEN_TEST_FIXTURES,
     )
-    assert result.exit_code == 0
+
+
+@pytest.mark.online
+def test_cli_with_pinned_requirements_file():
+    requirements_file = test_env.get_test_loc("pinned-requirements.txt")
+    expected_file = test_env.get_test_loc("pinned-requirements.txt-expected.json", must_exist=False)
+    check_requirements_resolution(
+        requirements_file=requirements_file,
+        expected_file=expected_file,
+        regen=REGEN_TEST_FIXTURES,
+    )
+
+
+def check_specs_resolution(
+    specifier,
+    expected_file,
+    extra_options=tuple(),
+    regen=REGEN_TEST_FIXTURES,
+):
+    result_file = test_env.get_temp_file("json")
+    options = ["--specifier", specifier, "--json", result_file]
+    options.extend(extra_options)
+    run_cli(options=options)
+    check_json_results(
+        result_file=result_file,
+        expected_file=expected_file,
+        regen=regen,
+    )
+
+
+def check_requirements_resolution(
+    requirements_file,
+    expected_file,
+    extra_options=tuple(),
+    regen=REGEN_TEST_FIXTURES,
+):
+    result_file = test_env.get_temp_file("json")
+    options = ["--requirement", requirements_file, "--json", result_file]
+    options.extend(extra_options)
+    run_cli(options=options)
+    check_json_results(
+        result_file=result_file,
+        expected_file=expected_file,
+        regen=regen,
+    )
+
+
+def check_json_results(result_file, expected_file, regen=REGEN_TEST_FIXTURES):
+    """
+    Check the ``result_file`` JSON results against the ``expected_file``
+    expected JSON results.
+
+    If ``regen`` is True the expected_file WILL BE overwritten with the new
+    results from ``results_file``. This is convenient for updating tests
+    expectations.
+    """
+    with open(result_file) as res:
+        results = json.load(res)
+
+    if regen:
+        with open(expected_file, "w") as reg:
+            json.dump(results, reg, indent=2, separators=(",", ": "))
+        expected = results
+    else:
+        with open(expected_file) as res:
+            expected = json.load(res)
+
+    assert results == expected
+
+
+def run_cli(options, cli=resolve_dependencies, expected_rc=0, env=None):
+    """
+    Run a command line resolution. Return a click.testing.Result object.
+    """
+
+    if not env:
+        env = dict(os.environ)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, options, catch_exceptions=False, env=env)
+
+    if result.exit_code != expected_rc:
+        output = result.output
+        error = f"""
+Failure to run:
+rc: {result.exit_code}
+python-inspector {options}
+output:
+{output}
+"""
+        assert result.exit_code == expected_rc, error
+    return result
