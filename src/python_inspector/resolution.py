1aeed1b9091f08a47df9c84b3adaea94867f825e
TrellixVulnTeam <charles.mcfarland@trellix.com>
2023-01-07 08:45:34 +0000

    Adding tarfile member sanitization to extractall()

--- src/python_inspector/resolution.py
+++ src/python_inspector/resolution.py
@@ -207,7 +207,26 @@ def get_sdist_file_path_from_filename(sdist):
     if sdist.endswith(".tar.gz"):
         sdist_file = sdist.rstrip(".tar.gz")
         with tarfile.open(os.path.join(utils_pypi.CACHE_THIRDPARTY_DIR, sdist)) as file:
-            file.extractall(
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner) 
+                
+            
+            safe_extract(file, os.path.join(utils_pypi.CACHE_THIRDPARTY_DIR,"extracted_sdists",sdist_file))
                 os.path.join(utils_pypi.CACHE_THIRDPARTY_DIR, "extracted_sdists", sdist_file)
             )
     elif sdist.endswith(".zip"):
