a89fcee109583a862b21b0b18b078fa47f62c05f
ukaj <ukaj869@gmail.com>
2025-03-11 09:35:23 +0100

    fix: Incorrect parsing of index_urls in requirements.txt
    Signed-off-by: ukaj <ukaj869@gmail.com>

--- src/python_inspector/api.py
+++ src/python_inspector/api.py
@@ -155,8 +155,12 @@ def resolve_dependencies(
     for req_file in requirement_files:
         deps = dependencies.get_dependencies_from_requirements(requirements_file=req_file)
         for extra_data in dependencies.get_extra_data_from_requirements(requirements_file=req_file):
-            index_urls = (*index_urls, *tuple(extra_data.get("extra_index_urls") or []))
-            index_urls = (*index_urls, *tuple(extra_data.get("index_url") or []))
+            extra_index_urls = extra_data.get("extra_index_urls") or []
+            index_url = extra_data.get("index_url") or []
+            if isinstance(extra_index_urls, list):
+                index_urls = (*index_urls, *tuple(extra_index_urls))
+            if isinstance(index_url, str):
+                index_urls = (*index_urls, index_url)
         direct_dependencies.extend(deps)
         package_data = [
             pkg_data.to_dict() for pkg_data in PipRequirementsFileHandler.parse(location=req_file)
