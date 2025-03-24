6ddcb58e5c8dba673a87cd44eac75e111f050225
Tushar Goel <tushar.goel.dav@gmail.com>
2025-03-24 14:11:55 +0530

    Fix formatting errors
    Signed-off-by: Tushar Goel <tushar.goel.dav@gmail.com>

--- tests/test_codestyle.py
+++ tests/test_codestyle.py
@@ -8,13 +8,13 @@
 #
 
 import subprocess
-import unittest
 import sys
+import unittest
+
 import pytest
 
 
 class BaseTests(unittest.TestCase):
-
     @pytest.mark.skipif(sys.version_info[:2] == (3, 12), reason="Skipping test for Python 3.12")
     def test_codestyle(self):
         """
